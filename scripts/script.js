08b1fb4681bd5a091df5e5606a96e1bc7f9a7451
AllanCarelli <142117112+AllanCarelli@users.noreply.github.com>
2026-08-07 14:19:55 -0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+const botao = document.getElementById("startQuestion");
+const ini = document.getElementById("startScreen");
+const quiz = document.getElementById("quizScreen");
+
+botao.addEventListener("click", function() {
+  if (ini.style.display == "block"){
+    ini.style.display = "none";
+    quiz.style.display = "block";
+  }
+}
+
+
+);
+function trocar() {
+  alert("nigger")
+}
