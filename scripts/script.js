36aaf802552ad78e0a7b430b4fc9d9d372cdd086
Thales Suzuki <thalesgimenes567@gmail.com>
2026-08-07 22:35:31 -0300

    w

--- scripts/script.js
+++ scripts/script.js
@@ -6,25 +6,18 @@ let answered = false;
 const botao = document.getElementById("startQuestion");
 const ini = document.getElementById("startScreen");
 const quiz = document.getElementById("quizScreen");
+const pergunta = document.getElementById("pergunta");
+const res1 = document.getElementById("resp1txt");
+const res2 = document.getElementById("resp2txt");
+const res3 = document.getElementById("resp3txt");
+const res4 = document.getElementById("resp4txt");
+const res1x = document.getElementById("resp1");
+const res2x = document.getElementById("resp2");
+const res3x = document.getElementById("resp3");
+const res4x = document.getElementById("resp4");
+const proxima = document.getElementById("next");
 
 let totalQuestions = 10;
-
-botao.addEventListener("click", function() {
-  if (ini.style.display == "block"){
-    ini.style.display = "none";
-    quiz.style.display = "block";
-  }
-}
-);
-
-function shuffle(arr) {
- for (let i = arr.length - 1; i > 0; i--) {
- const j = Math.floor(Math.random() * (i + 1));
- [arr[i], arr[j]] = [arr[j], arr[i]];
- }
- return arr;
-}
-
 
 const allQuestions = [ {q: "Qual é o maior mamífero terrestre?", 
 opts: ["Elefante africano", "Rinoceronte", "Girafa", "Hipopótamo"],
@@ -63,3 +56,44 @@ info: "As plantas usam CO₂ na fotossíntese para produzir seu alimento." },
   ans: 1,
   info: "legal5"
 }]
+
+botao.addEventListener("click", function() {
+  let score = 0;
+  let currentQ = 0;
+  if (ini.style.display == "block"){
+    ini.style.display = "none";
+    quiz.style.display = "block";
+  }
+
+  let questao = shuffle(allQuestions);
+  pergunta.innerHTML = questao[0]["q"];
+  res1.innerHTML = questao[0]["opts"][0];
+  res2.innerHTML = questao[0]["opts"][1];
+  res3.innerHTML = questao[0]["opts"][2];
+  res4.innerHTML = questao[0]["opts"][3];
+  res1x.setAttribute("value", questao[0]["opts"][0]);
+  res2x.setAttribute("value", questao[0]["opts"][1]);
+  res3x.setAttribute("value", questao[0]["opts"][2]);
+  res4x.setAttribute("value", questao[0]["opts"][3]);
+}); 
+proxima.addEventListener("click", function(){
+  questao = shuffle(allQuestions);
+  pergunta.innerHTML = questao[0]["q"];
+  res1.innerHTML = questao[0]["opts"][0];
+  res2.innerHTML = questao[0]["opts"][1];
+  res3.innerHTML = questao[0]["opts"][2];
+  res4.innerHTML = questao[0]["opts"][3];
+  res1x.setAttribute("value", questao[0]["opts"][0]);
+  res2x.setAttribute("value", questao[0]["opts"][1]);
+  res3x.setAttribute("value", questao[0]["opts"][2]);
+  res4x.setAttribute("value", questao[0]["opts"][3]);
+})
+
+
+  function shuffle(arr) {
+ for (let i = arr.length - 1; i > 0; i--) {
+ const j = Math.floor(Math.random() * (i + 1));
+ [arr[i], arr[j]] = [arr[j], arr[i]];
+ }
+ return arr;
+}
