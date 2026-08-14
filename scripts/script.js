f189ee6cef67c1f8164b5f9536c01da61bb20156
AllanCarelli <allan.nunes.carelli@gmail.com>
2026-08-14 20:01:20 -0300

    +

--- scripts/script.js
+++ scripts/script.js
@@ -16,8 +16,24 @@ const res2x = document.getElementById("resp2");
 const res3x = document.getElementById("resp3");
 const res4x = document.getElementById("resp4");
 const proxima = document.getElementById("next");
+const end = document.getElementById("endScreen");
 
 let totalQuestions = 10;
+
+function atualizarRespostas(currentQ){
+  pergunta.innerHTML = questao[currentQ]["q"];
+  res1.innerHTML = questao[currentQ]["opts"][0];
+  res2.innerHTML = questao[currentQ]["opts"][1];
+  res3.innerHTML = questao[currentQ]["opts"][2];
+  res4.innerHTML = questao[currentQ]["opts"][3];
+  res1x.setAttribute("value", questao[currentQ]["opts"][0]);
+  res2x.setAttribute("value", questao[currentQ]["opts"][1]);
+  res3x.setAttribute("value", questao[currentQ]["opts"][2]);
+  res4x.setAttribute("value", questao[currentQ]["opts"][3]);
+}
+
+
+
 
 const allQuestions = [ {q: "Qual é o maior mamífero terrestre?", 
 opts: ["Elefante africano", "Rinoceronte", "Girafa", "Hipopótamo"],
@@ -55,45 +71,75 @@ info: "As plantas usam CO₂ na fotossíntese para produzir seu alimento." },
   opts: ["re1", "res2", "res3", "res4"],
   ans: 1,
   info: "legal5"
+},
+{q: "pergunta6",
+  opts: ["res1", "res2", "re3", "res4"],
+  ans:3,
+  info:"ok"
+},
+{q: "pergunta7",
+  opts: ["res1", "res2", "re3", "res4"],
+  ans:3,
+  info:"ok"
 }]
 
+function shuffle(arr) {
+  let ar = [];
+ for (let i = arr.length - 1; i > 0; i--) {
+  let rand = Math.floor(Math.random(0,10) * 10)
+  while (ar.includes(rand)){
+    rand = Math.floor(Math.random(0,10) * 10)
+  }
+  alert(ar)
+  alert(ar.includes(rand))
+  ar.push(rand)
+ const j = rand;
+ [arr[i], arr[j]] = [arr[j], arr[i]];
+ alert(`${j}`)
+ }
+ return arr;
+}
+
+
+
+
+let questao = shuffle(allQuestions);
+
+function respCorreta(resp,currentQ,pos){
+  
+  if (resp == questao[currentQ]["opts"][pos]){
+    return true
+  }
+  
+  return false
+}
+
 botao.addEventListener("click", function() {
-  let score = 0;
-  let currentQ = 0;
+  score = 0;
+  currentQ = 0;
   if (ini.style.display == "block"){
     ini.style.display = "none";
     quiz.style.display = "block";
   }
+  atualizarRespostas(currentQ)
+});
 
-  let questao = shuffle(allQuestions);
-  pergunta.innerHTML = questao[0]["q"];
-  res1.innerHTML = questao[0]["opts"][0];
-  res2.innerHTML = questao[0]["opts"][1];
-  res3.innerHTML = questao[0]["opts"][2];
-  res4.innerHTML = questao[0]["opts"][3];
-  res1x.setAttribute("value", questao[0]["opts"][0]);
-  res2x.setAttribute("value", questao[0]["opts"][1]);
-  res3x.setAttribute("value", questao[0]["opts"][2]);
-  res4x.setAttribute("value", questao[0]["opts"][3]);
-}); 
+
 proxima.addEventListener("click", function(){
-  questao = shuffle(allQuestions);
-  pergunta.innerHTML = questao[0]["q"];
-  res1.innerHTML = questao[0]["opts"][0];
-  res2.innerHTML = questao[0]["opts"][1];
-  res3.innerHTML = questao[0]["opts"][2];
-  res4.innerHTML = questao[0]["opts"][3];
-  res1x.setAttribute("value", questao[0]["opts"][0]);
-  res2x.setAttribute("value", questao[0]["opts"][1]);
-  res3x.setAttribute("value", questao[0]["opts"][2]);
-  res4x.setAttribute("value", questao[0]["opts"][3]);
+  let selected = document.querySelector('input[name="resp"]:checked')?.value || null;
+  alert(currentQ)
+  if (currentQ >= 10){
+    quiz.style.display = "none";
+    end.style.display = "block";
+    document.getElementById("resultado").innerHTML = `texot: ${score}`;
+  }
+  if(respCorreta(selected,currentQ,questao[currentQ]["ans"])){
+    score += 1;
+  }
+  currentQ += 1;
+  atualizarRespostas(currentQ)
+
 })
 
 
-  function shuffle(arr) {
- for (let i = arr.length - 1; i > 0; i--) {
- const j = Math.floor(Math.random() * (i + 1));
- [arr[i], arr[j]] = [arr[j], arr[i]];
- }
- return arr;
-}
+
